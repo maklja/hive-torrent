dbf5089b46f08f6b3d7677a9542a3ff840dd7199
Bojan Makljenovic <maklja90@gmail.com>
2024-07-05 16:48:34 +0200

    Init commit

---
+++ lib/hive_torrent_application.ex
@@ -0,0 +1,18 @@
+defmodule HiveTorrentApplication do
+  @moduledoc """
+  Documentation for `HiveTorrentApplication`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HiveTorrentApplication.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hive_torrent_application_test.exs
@@ -0,0 +1,8 @@
+defmodule HiveTorrentApplicationTest do
+  use ExUnit.Case
+  doctest HiveTorrentApplication
+
+  test "greets the world" do
+    assert HiveTorrentApplication.hello() == :world
+  end
+end

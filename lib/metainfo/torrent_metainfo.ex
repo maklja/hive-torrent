5d935c51949fe3597cad7dd6d07e5313dff14f02
Bojan Makljenovic <maklja90@gmail.com>
2024-07-29 23:28:29 +0200

    Next

--- lib/metainfo/torrent_metainfo.ex
+++ lib/metainfo/torrent_metainfo.ex
@@ -3,27 +3,83 @@ defmodule HiveTorrent.TorrentInfoMeta do
 end
 
 defmodule HiveTorrent.Torrent do
-  defstruct [:announce, :name, :announce_list, :comment, :created_by, :creation_date, :info]
+  defstruct [
+    :trackers,
+    :name,
+    :comment,
+    :created_by,
+    :creation_date,
+    :files,
+    :size,
+    :piece_length
+  ]
 
   def parse(file_path) do
     with {:ok, data} <- File.read(file_path),
          {:ok, torrent_data} <- HiveTorrent.Bencode.Parser.parse(data),
-         {:ok, announce} <- Map.fetch(torrent_data, "announce"),
+         {:ok, trackers} <- get_trackers(torrent_data),
          {:ok, info} <- Map.fetch(torrent_data, "info"),
-         announce_list <- Map.get(torrent_data, "announce-list", []),
+         {:ok, name} <- Map.fetch(info, "name"),
+         {:ok, piece_length} <- Map.fetch(info, "piece length"),
          comment <- Map.get(torrent_data, "comment", ""),
          created_by <- Map.get(torrent_data, "created by", ""),
-         creation_date <- Map.get(torrent_data, "creation date") do
+         {:ok, creation_date} <- Map.get(torrent_data, "creation date", 0) |> DateTime.from_unix() do
+      files = get_files(info, name)
+      files_size = Enum.reduce(files, 0, &(elem(&1, 1) + &2))
+      pieces_hashes = get_hashes(Map.get(info, "pieces"))
+
       IO.inspect(torrent_data)
-      IO.inspect(byte_size(Map.get(Map.get(torrent_data, "info"), "pieces")))
+
+      test =
+        {:ok,
+         %__MODULE__{
+           trackers: trackers,
+           name: name,
+           comment: comment,
+           created_by: created_by,
+           creation_date: creation_date,
+           files: files,
+           size: files_size,
+           piece_length: piece_length
+         }}
+
+      IO.inspect(test)
+
+      # IO.inspect(byte_size(Map.get(Map.get(torrent_data, "info"), "pieces")))
     end
 
-    # with {:ok, file} <- File.read(path),
-    #      {:ok, torrent} <- Bento.decode(file),
-    #      {:ok, info} <- Map.fetch(torrent, "info"),
-    #      {:ok, name} <- Map.fetch(info, "name"),
-    #      {:ok, pieces} <- Map.fetch(info, "pieces"),
-    #      {:ok, piece_length} <- Map.fetch(info, "piece length"),
-    #      {:ok, trackers} <- get_trackers(torrent) do
+    # %Torrent{
+    #  name: name,
+    #  info_hash: info_hash,
+    #  files: files,
+    #  size: size,
+    #  trackers: trackers,
+    #  piece_length: piece_length,
+    #  pieces: piece_map
+    #  }
+  end
+
+  defp get_trackers(%{"announce-list" => announce_list}), do: {:ok, List.flatten(announce_list)}
+
+  defp get_trackers(%{"announce" => announce}), do: {:ok, [announce]}
+
+  defp get_trackers(_), do: {:error, :no_trackers}
+
+  defp get_files(%{"files" => files}, name) do
+    Enum.map(files, fn %{"length" => length, "path" => path} ->
+      {Path.join(name, path), length}
+    end)
+  end
+
+  defp get_files(%{"length" => length}, name) do
+    [{name, length}]
+  end
+
+  defp get_hashes(hash, num \\ 0, acc \\ %{})
+
+  defp get_hashes("", _num, acc), do: acc
+
+  defp get_hashes(<<hash::bytes-size(20), rest::binary>>, num, acc) do
+    get_hashes(rest, num + 1, Map.put(acc, num, hash))
   end
 end

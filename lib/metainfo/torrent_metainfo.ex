b93c5543df3d47dc31d6657bffaf574acca4118e
Bojan Makljenovic <maklja90@gmail.com>
2024-07-30 16:59:16 +0200

    Next

--- lib/metainfo/torrent_metainfo.ex
+++ lib/metainfo/torrent_metainfo.ex
@@ -28,7 +28,14 @@ defmodule HiveTorrent.Torrent do
       files_size = Enum.reduce(files, 0, &(elem(&1, 1) + &2))
       pieces_hashes = get_hashes(Map.get(info, "pieces"))
 
-      IO.inspect(torrent_data)
+      files_1 = [{"Test1", 73}, {"Test2", 37}, {"Test3", 40}]
+
+      files_pieces =
+        files_1
+        |> find_files_pieces(40)
+
+      # files_pieces = file_pieces(40, 40)
+      IO.inspect(files_pieces)
 
       test =
         {:ok,
@@ -43,7 +50,7 @@ defmodule HiveTorrent.Torrent do
            piece_length: piece_length
          }}
 
-      IO.inspect(test)
+      # IO.inspect(test)
 
       # IO.inspect(byte_size(Map.get(Map.get(torrent_data, "info"), "pieces")))
     end
@@ -83,15 +90,60 @@ defmodule HiveTorrent.Torrent do
     get_hashes(rest, num + 1, Map.put(acc, num, hash))
   end
 
-  defp file_pieces(file_size, piece_length, piece_num \\ 0, pieces \\ [])
+  defp find_files_pieces(files, piece_length, last_piece \\ {-1, 0, 0}, pieces \\ [])
 
-  defp file_pieces(file_size, piece_length, piece_num, pieces) do
-    file_rem = file_size - piece_length * piece_num
+  defp find_files_pieces([], _piece_length, _last_piece, pieces), do: Enum.reverse(pieces)
+
+  defp find_files_pieces([file | rest], piece_length, last_piece, pieces) do
+    {file_name, file_size} = file
+    {piece_num, piece_offset, piece_size} = last_piece
+
+    case file_pieces(file_size, piece_length, piece_offset + piece_size, piece_num + 1) do
+      {new_pieces, nil} ->
+        last_piece = List.first(new_pieces)
+        new_pieces = Enum.map(new_pieces, &Tuple.append(&1, file_name))
+        find_files_pieces(rest, piece_length, last_piece, new_pieces ++ pieces)
+
+      {new_pieces, last_piece} ->
+        new_pieces = Enum.map(new_pieces, &Tuple.append(&1, file_name))
+        # new_pieces = if length(rest) > 0, do: [last_piece | new_pieces], else: new_pieces
+        find_files_pieces(rest, piece_length, last_piece, new_pieces ++ pieces)
+    end
   end
 
-  # 150 40
-  # 73 27 50
-  # 40 33
-  # 27
-  # 40 10
+  defp file_pieces(file_size, piece_length, piece_offset, piece_num, pieces \\ [])
+
+  defp file_pieces(0, _piece_length, _piece_offset, _piece_num, pieces) do
+    {pieces, nil}
+  end
+
+  defp file_pieces(file_size, piece_length, piece_offset, piece_num, pieces) when file_size > 0 do
+    file_size_rem = file_size - piece_length
+
+    cond do
+      file_size_rem >= 0 ->
+        piece = {piece_num, piece_offset, piece_length}
+
+        file_pieces(file_size_rem, piece_length, piece_offset + piece_length, piece_num + 1, [
+          piece | pieces
+        ])
+
+      true ->
+        piece_size = piece_length + file_size_rem
+        piece = {piece_num, piece_offset, piece_size}
+
+        file_pieces(file_size_rem, piece_length, piece_offset + piece_size, piece_num, [
+          piece | pieces
+        ])
+    end
+  end
+
+  defp file_pieces(file_size, _piece_length, piece_offset, piece_num, pieces)
+       when file_size < 0 do
+    last_piece = {piece_num, piece_offset, abs(file_size)}
+    {pieces, last_piece}
+  end
+
+  # you don't need last piece, you can just use offset
+  # 40 - rem(80, 40)
 end

05aa11f872ad1510bb04825d0a8a926431df1eef
Bojan Makljenovic <maklja90@gmail.com>
2024-08-02 10:34:25 +0200

    Next

--- lib/metainfo/bencode_serializer.ex
+++ lib/metainfo/bencode_serializer.ex
@@ -36,18 +36,31 @@ defmodule HiveTorrent.Bencode.Serializer do
   Returns Bencode string value, otherwise raises HiveTorrent.Bencode.SerializeError
 
   ## Examples
-      iex> HiveTorrent.Bencode.Serializer.encode(1)
+      iex> HiveTorrent.Bencode.Serializer.encode!(1)
       "i1e"
 
-      iex> HiveTorrent.Bencode.Serializer.encode(%{test: 999})
+      iex> HiveTorrent.Bencode.Serializer.encode!(%{test: 999})
       "d4:testi999ee"
 
-      iex> HiveTorrent.Bencode.Serializer.encode(9.99)
+      iex> HiveTorrent.Bencode.Serializer.encode!(9.99)
       ** (HiveTorrent.Bencode.SerializeError) Unsupported types: Float
   """
   @spec encode(serializable()) :: binary() | no_return()
+  def encode!(value) do
+    case encode(value) do
+      {:ok, encoded_value} -> encoded_value
+      {:error, e} -> raise e
+    end
+  end
+
+  @spec encode(serializable()) :: binary() | no_return()
   def encode(value) do
-    value |> SerializerProtocol.encode() |> IO.iodata_to_binary()
+    try do
+      encoded_value = value |> SerializerProtocol.encode() |> IO.iodata_to_binary()
+      {:ok, encoded_value}
+    rescue
+      e -> {:error, e}
+    end
   end
 end
 
--- test/metainfo/torrent_metainfo_test.exs
+++ test/metainfo/torrent_metainfo_test.exs
@@ -4,7 +4,8 @@ defmodule HiveTorrent.TorrentTest do
 
   test "parse valid torrent with length" do
     torrent_path = Path.join([__DIR__, ~c"fixtures", ~c"torrent", ~c"valid.torrent"])
-    File.read(torrent_path) |> IO.inspect()
+    assert torrent_path === __DIR__ <> "/fixtures/torrent/valid.torrent"
+    # File.read(torrent_path) |> IO.inspect()
     # {value, expected} = fixture.test_numbers.positive_int
     # assert value |> IO.iodata_to_binary() |> Parser.parse() == {:ok, expected}
   end
